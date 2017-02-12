d618234278e0f30583ea0eb5f7ab269fc6804cf6
Faras Momin <famomin@utexas.edu>
2017-02-12 17:50:01 -0600

    minor change on comment

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,16 +6,16 @@ module.exports = function(app) {
 
   //root route for home page
   app.get('/', function(req, res) {
-  	res.sendFile(path.join(__dirname, "app/public/home.html"));
+  	res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 
   // /survey route to let user fill out survey
   app.get('/survey', function(req, res) {
-  	res.sendFile(path.join(__dirname, "app/public/survey.html"));
+  	res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.use(function(req, res) {
-  	res.sendFile(path.join(__dirname, "app/public/home.html"));
+  	res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 };

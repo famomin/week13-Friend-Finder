55fd5b8173d4bd1be8781e5758334f2dbd217425
Faras Momin <famomin@utexas.edu>
2017-02-12 17:49:15 -0600

    added logic to find best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,36 @@
-var userdata = require("..data/friends");
+//requireing the data from friends.js file
+var userData = require("../data/friends");
 
 module.exports = function(app) {
   // when /api/tables is visited, display the Friends available array
   app.get('/api/tables', function(req, res) {
-  	res.send(peopleAvail);
+  	res.send(userData);
   });
 
+  //push new person to the array and show it on api/tables. Applying logic for best match.
   app.post('/api/tables', function(req, res) {
-  	res.send(peopleAvail);
-  });
-};
+    var newFriend = req.body;
+    userData.push(newFriend); //pushing new friedn to api tables (array)
+
+    var lowScore = 50; //50 imaginary number. Difference of two people is not possible to get to 50.
+    var closeMatch;
+
+    //double for loop. first loop to run through each exisiting person on array.
+    for (var i = 0; i < userData.length - 1; i++){
+      var differenceScore = 0;
+
+      //second loop to run though all the inpus from users.
+      for (var j = 0; j < userData[i].length; j++ ) {
+        differenceScore = differenceScore + Math.abs(userData[i].scores[j] - newFriend.score[j]);
+      } //closing j for loop
+
+      //checking if differenceScore is less than previous low score or not.
+      if(differenceScore < lowScore){
+        lowScore = differenceScore;
+        closeMatch = i;
+      } //closing if statment
+    } //closing i for loop
+
+    res.json(userData[closeMatch]);
+  }); //closing post route
+}; // clsoing module.exports function

47fed2ff8bd9b3be46961d1390339998eb466034
Faras Momin <famomin@utexas.edu>
2017-02-12 20:59:22 -0600

    four people with new pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 //list of potential people
 var peopleAvail = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [ "5", "1", "4", "4", "5", "1", "2", "5", "4", "1" ]
+    "name": "Donald J. Trump",
+    "photo": "http://static6.businessinsider.com/image/55918b77ecad04a3465a0a63/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners.jpg",
+    "scores": [ "1", "1", "4", "4", "5", "1", "2", "5", "4", "1" ]
   },
   {
-    "name": "Jacob Deming",
-    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "name": "Ted Cruz",
+    "photo": "http://media.salon.com/2015/12/ted_cruz22-620x412.jpg",
     "scores": [ "4", "2", "5", "1", "3", "2", "2", "1", "3", "2" ]
   },
   {
-    "name": "Jeremiah Scanlon",
-    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "name": "Hilary Clinton",
+    "photo": "https://mises.org/sites/default/files/23158392405_d3e3b60cb8_o.jpg",
     "scores": [ "5", "2", "2", "2", "4", "1", "3", "2", "5", "5" ]
   },
   {
-    "name": "Louis T. Delia",
+    "name": "Bernie Sanders",
     "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    "scores": [ "3", "3", "4", "2", "2", "1", "3", "2", "2", "3" ]
+    "scores": [ "4", "3", "4", "2", "2", "1", "3", "2", "2", "3" ]
   }
 ];
 
